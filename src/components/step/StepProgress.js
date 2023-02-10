1c0d77dc87eabddd07f498599754a444c566720f
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-02-10 11:00:05 +0800

    feat: restructure StepProgress.js by using JSX
    將 StepProgress 區塊的 html 以 components 搭配 JSX 語法撰寫

---
+++ src/components/step/StepProgress.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+function RegisterContainer({ children }) {
+  return (
+    <section
+      className="register-container col col-lg-6 col-sm-12"
+      data-phase="1"
+      data-total-price="0"
+    >
+      {children}
+    </section>
+  );
+}
+
+function ProgressContainer({ children }) {
+  return (
+    <section className="progress-container col col-12">{children}</section>
+  );
+}
+
+function Title() {
+  return <h2 className="register-title col col-12">結帳</h2>;
+}
+
+function ProgressGroup({ phase, order, label }) {
+  return (
+    <span className="progress-group" data-phase={phase}>
+      <span className="progress-icon">
+        <span className="text">{order}</span>
+        <svg className="icon cursor-point">
+          {/* <use xlink:href="#svg-icon-pg-complete"></use> */}
+        </svg>
+      </span>
+      <span className="progress-label">{label}</span>
+    </span>
+  );
+}
+
+function ProgressBar({ order }) {
+  return <span className="progress-bar" data-order={order}></span>;
+}
+
+export default function () {
+  return (
+    <RegisterContainer>
+      {/* <!-- register-title --> */}
+      <Title />
+
+      {/* <!-- register-progress --> */}
+      <ProgressContainer>
+        <ProgressGroup phase="address" order="1" label="寄送地址" />
+
+        <ProgressBar order="1" />
+
+        <ProgressGroup phase="shipping" order="2" label="運送方式" />
+
+        <ProgressBar order="2" />
+
+        <ProgressGroup phase="credit-card" order="3" label="付款資訊" />
+      </ProgressContainer>
+    </RegisterContainer>
+  );
+}
