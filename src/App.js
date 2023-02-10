2525f9f95ff5b54dc2db048b7e4515bf03c8ac39
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-02-10 15:40:41 +0800

    feat: restructure ProgressControl.js by using JSX
    將 ProgressControl 區塊的 html 以 components 搭配 JSX 語法撰寫

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import StepProgress from "./components/step/StepProgress";
 import Step1 from "./components/step/Step1";
+import ProgressControl from "./components/step/ProgressControl";
 
 export default function () {
   return (
     <>
       <StepProgress />
       <Step1 />
+      <ProgressControl />
     </>
   );
 }
--- src/components/step/ProgressControl.js
+++ src/components/step/ProgressControl.js
@@ -1,1 +1,38 @@
 import React from "react";
+import iconRightArrow from "../../icons/icon_right_arrow.svg";
+import iconLeftArrow from "../../icons/icon_left_arrow.svg";
+
+function Button({ way, content, source }) {
+  return (
+    <button className={way}>
+      {content}
+      <img src={source} alt="" className="cursor-point" />
+    </button>
+  );
+}
+
+export default function ProgressControl() {
+  return (
+    <section className="progress-control-container col col-lg-6 col-sm-12">
+      {/* 第一步 */}
+      <section className="button-group col col-12" data-phase="address">
+        <Button way="next" content="下一步" source={iconRightArrow} />
+      </section>
+      {/* 第一步end */}
+
+      {/* 第二步 */}
+      <section className="button-group col col-12" data-phase="shipping">
+        <Button way="prev" content="上一步" source={iconLeftArrow} />
+        <Button way="next" content="下一步" source={iconRightArrow} />
+      </section>
+      {/* 第二步end */}
+
+      {/* 第三步 */}
+      <section className="button-group col col-12" data-phase="credit-card">
+        <Button way="prev" content="上一步" source={iconLeftArrow} />
+        <button className="next">確認下單</button>
+      </section>
+      {/* 第三步end */}
+    </section>
+  );
+}
