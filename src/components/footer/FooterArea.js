c1bade3c7722077ffc1add3dc384691e1b9b9bc1
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-02-11 20:31:17 +0800

    style: 調整其他未實作區域的背景色
    讓觀看者視覺上不會太刺眼

--- src/components/footer/FooterArea.js
+++ src/components/footer/FooterArea.js
@@ -2,7 +2,7 @@ export default function FooterArea() {
   return (
     <div
       className=""
-      style={{ backgroundColor: "yellow", width: "100%", height: "300px" }}
+      style={{ backgroundColor: "grey", width: "100%", height: "300px" }}
     >
       FooterArea
     </div>
--- src/components/header/HeaderArea.js
+++ src/components/header/HeaderArea.js
@@ -2,7 +2,7 @@ export default function HeaderArea() {
   return (
     <div
       className=""
-      style={{ backgroundColor: "orange", width: "100%", height: "50px" }}
+      style={{ backgroundColor: "grey", width: "100%", height: "50px" }}
     >
       HeaderArea
     </div>
