04dbd39c11f7ba0681e54162c59e0bb71c4709fe
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-02-11 18:00:03 +0800

    feat: Add CSS to ProgressControl.js
    完成 ProgressControl.js 的樣式排版

--- src/components/main/step/ProgressControl.js
+++ src/components/main/step/ProgressControl.js
@@ -1,10 +1,11 @@
 import React from "react";
 import iconRightArrow from "../../../icons/icon_right_arrow.svg";
 import iconLeftArrow from "../../../icons/icon_left_arrow.svg";
+import styles from "./styles/progress_control.module.scss";
 
 function Button({ way, content, source }) {
   return (
-    <button className={way}>
+    <button className={styles[way]}>
       {content}
       <img src={source} alt="" className="cursor-point" />
     </button>
@@ -13,29 +14,35 @@ function Button({ way, content, source }) {
 
 export default function ProgressControl() {
   return (
-    <>
-      <div style={{ width: "100%", borderTop: "1px solid black" }} />
-      <section className="progress-control-container col col-lg-6 col-sm-12">
-        {/* 第一步 */}
-        <section className="button-group col col-12" data-phase="address">
-          <Button way="next" content="下一步" source={iconRightArrow} />
-        </section>
-        {/* 第一步end */}
-
-        {/* 第二步 */}
-        <section className="button-group col col-12" data-phase="shipping">
-          <Button way="prev" content="上一步" source={iconLeftArrow} />
-          <Button way="next" content="下一步" source={iconRightArrow} />
-        </section>
-        {/* 第二步end */}
-
-        {/* 第三步 */}
-        <section className="button-group col col-12" data-phase="credit-card">
-          <Button way="prev" content="上一步" source={iconLeftArrow} />
-          <button className="next">確認下單</button>
-        </section>
-        {/* 第三步end */}
+    <section className="progress-control-container col  col-12">
+      {/* 第一步 */}
+      <section
+        className={`${styles.button_group} col col-12`}
+        data-phase="address"
+      >
+        <Button way="next" content="下一步" source={iconRightArrow} />
       </section>
-    </>
+      {/* 第一步end */}
+
+      {/* 第二步 */}
+      <section
+        className={`${styles.button_group} col col-12 ${styles.d_none}`}
+        data-phase="shipping"
+      >
+        <Button way="prev" content="上一步" source={iconLeftArrow} />
+        <Button way="next" content="下一步" source={iconRightArrow} />
+      </section>
+      {/* 第二步end */}
+
+      {/* 第三步 */}
+      <section
+        className={`${styles.button_group} col col-12 ${styles.d_none}`}
+        data-phase="credit-card"
+      >
+        <Button way="prev" content="上一步" source={iconLeftArrow} />
+        <button className={styles.next}>確認下單</button>
+      </section>
+      {/* 第三步end */}
+    </section>
   );
 }
